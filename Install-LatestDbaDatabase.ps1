99fdef2a57fbeaf24dd9672779a990434ec6b4a9
Andy ᴹᴬᴸᴸᴼᴺ (ᴀᴍ²) <amtwo@users.noreply.github.com>
2017-09-22 11:02:29 -0400

    Add PowerShell Installer
    PowerShell installer will create/update code.

---
+++ Install-LatestDbaDatabase.ps1
@@ -0,0 +1,68 @@
+<#
+.SYNOPSIS
+Installs or updates DBA database to the latest version
+ 
+.DESCRIPTION
+This function will create a DBA database if it does not already exist, and install the latest code. 
+
+This depends on having the full, latest version of the full repo https://github.com/amtwo/dba-database
+
+All dependent .sql files are itempotent:
+* Table.sql scripts are written to create if not exists. Changes are maintained similarly as conditional ALTERs.
+* code.sql scripst are written to create a stub, then alter with actual code.
+
+.PARAMETER instanceName
+An array of instance names
+ 
+.EXAMPLE
+Install-LatestDbaDatabase AM2Prod
+ 
+
+.NOTES
+AUTHOR: Andy Mallon
+DATE: 20170922
+#>
+ 
+[CmdletBinding()]
+param ([string[]]$instanceName)
+
+# Process servers in a loop. I could do this parallel, but doing it this way is fast enough for me.
+foreach($instance in $instanceName) {
+    Write-Verbose "**************************************************************"
+    Write-Verbose "                           $instance"
+    Write-Verbose "**************************************************************"
+    #Create the database - SQL Script contains logic to be conditional & not clobber existing database
+    Write-Verbose "`n        ***Creating Database if necessary `n"
+    Invoke-Sqlcmd -ServerInstance $instance -Database master -InputFile .\create-database.sql
+
+    #Create tables first
+    Write-Verbose "`n        ***Creating/Updating Tables `n"
+    $fileList = Get-ChildItem -Path .\tables -Recurse
+    Foreach ($file in $fileList){
+        Write-Verbose $file.FullName
+        Invoke-Sqlcmd -ServerInstance $instance -Database DBA -InputFile $file.FullName
+    }
+    #Then scalar functions
+    Write-Verbose "`n        ***Creating/Updating Scalar Functions `n"
+    $fileList = Get-ChildItem -Path .\functions-scalar -Recurse
+    Foreach ($file in $fileList){
+        Write-Verbose $file.FullName
+        Invoke-Sqlcmd -ServerInstance $instance -Database DBA -InputFile $file.FullName
+    }
+    #Then TVFs
+    Write-Verbose "`n        ***Creating/Updating Table-Valued Functions `n"
+    $fileList = Get-ChildItem -Path .\functions-tvfs -Recurse
+    Foreach ($file in $fileList){
+        Write-Verbose $file.FullName
+        Invoke-Sqlcmd -ServerInstance $instance -Database DBA -InputFile $file.FullName
+    }
+    #Then Procedures
+    Write-Verbose "`n        ***Creating/Updating Stored Procedures `n"
+    $fileList = Get-ChildItem -Path .\stored-procedures -Recurse -Filter *.sql
+    Foreach ($file in $fileList){
+        Write-Verbose $file.FullName
+        Invoke-Sqlcmd -ServerInstance $instance -Database DBA -InputFile $file.FullName
+    }
+
+#That's it!
+}
--- create-database.sql
+++ create-database.sql
@@ -1,21 +1,21 @@
-USE master
-GO
 --Create DB
-CREATE DATABASE [DBA];
-ALTER DATABASE  [DBA] ADD FILEGROUP [DATA];
---Need to use dynamic SQL to ensure we put this data file in the right spot dynamically
-DECLARE @sql nvarchar(max);
-SELECT @sql = N'ALTER DATABASE  [DBA] ADD FILE (NAME=''DBA_data'', FILENAME=''' 
-                + LEFT(physical_name,LEN(physical_name)-CHARINDEX('\',REVERSE(physical_name))+1) 
-                + N'DBA_data.ndf' + N''') TO FILEGROUP [DATA];'
-FROM sys.master_files 
-WHERE database_id = db_id('DBA')
-AND file_id = 1;
-EXEC sp_executesql @statement = @sql;
---And now finish up creating it
-ALTER DATABASE  [DBA] MODIFY FILEGROUP [DATA] DEFAULT;
-ALTER DATABASE  [DBA] SET READ_COMMITTED_SNAPSHOT ON;
-GO
---set sa as owner
-ALTER AUTHORIZATION ON database::DBA TO sa;
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DBA')
+BEGIN
+    CREATE DATABASE [DBA];
+    ALTER DATABASE  [DBA] ADD FILEGROUP [DATA];
+    --Need to use dynamic SQL to ensure we put this data file in the right spot dynamically
+    DECLARE @sql nvarchar(max);
+    SELECT @sql = N'ALTER DATABASE  [DBA] ADD FILE (NAME=''DBA_data'', FILENAME=''' 
+                    + LEFT(physical_name,LEN(physical_name)-CHARINDEX('\',REVERSE(physical_name))+1) 
+                    + N'DBA_data.ndf' + N''') TO FILEGROUP [DATA];'
+    FROM sys.master_files 
+    WHERE database_id = db_id('DBA')
+    AND file_id = 1;
+    EXEC sp_executesql @statement = @sql;
+    --And now finish up creating it
+    ALTER DATABASE  [DBA] MODIFY FILEGROUP [DATA] DEFAULT;
+    ALTER DATABASE  [DBA] SET READ_COMMITTED_SNAPSHOT ON;
+    --set sa as owner
+    ALTER AUTHORIZATION ON database::DBA TO sa;
+END
 GO
