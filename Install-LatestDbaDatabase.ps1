ad2fe1ab4485bce6320a3767491f092d630cb806
Andy Mallon <amallon@wayfair.com>
2020-05-01 09:07:25 -0400

    Resolves #7 & #8

--- Install-LatestDbaDatabase.ps1
+++ Install-LatestDbaDatabase.ps1
@@ -11,8 +11,17 @@ All dependent .sql files are itempotent:
 * Table.sql scripts are written to create if not exists. Changes are maintained similarly as conditional ALTERs.
 * code.sql scripst are written to create a stub, then alter with actual code.
 
-.PARAMETER instanceName
+.PARAMETER InstanceName
 An array of instance names
+
+.PARAMETER DatabaseName
+By default, this will be installed in a database called "DBA". If you want to install my DBA database with
+a different name, specify it here.
+
+.PARAMETER SkipOSS
+By default, this installer assumes that you've got the open source stuff in the right spot. If you don't
+want to install those packages, just pass in $true for this, and it'll skip all of them.
+
  
 .EXAMPLE
 Install-LatestDbaDatabase AM2Prod
@@ -26,66 +35,77 @@ COPYRIGHT: This code is licensed as part of Andy Mallon's DBA Database. https://
 #>
  
 [CmdletBinding()]
-param ([string[]]$instanceName)
+param (
+    [Parameter(Position=0,mandatory=$true)]
+        [string[]]$InstanceName,
+    [Parameter(Position=1,mandatory=$false)]
+        [string]$DatabaseName = 'DBA',
+    [Parameter(Position=2,mandatory=$false)]
+        [boolean]$SkipOSS = $false
+    )
 
 # Process servers in a loop. I could do this parallel, but doing it this way is fast enough for me.
-foreach($instance in $instanceName) {
+foreach($instance in $InstanceName) {
     Write-Verbose "**************************************************************"
     Write-Verbose "                           $instance"
     Write-Verbose "**************************************************************"
     #Create the database - SQL Script contains logic to be conditional & not clobber existing database
     Write-Verbose "`n        ***Creating Database if necessary `n"
-    Invoke-Sqlcmd -ServerInstance $instance -Database master -InputFile .\create-database.sql
+    Invoke-Sqlcmd -ServerInstance $instance -Database master -InputFile .\create-database.sql -Variable "DbName=$($DatabaseName)"
 
     #Create tables first
     Write-Verbose "`n        ***Creating/Updating Tables `n"
     $fileList = Get-ChildItem -Path .\tables -Recurse
     Foreach ($file in $fileList){
         Write-Verbose $file.FullName
-        Invoke-Sqlcmd -ServerInstance $instance -Database DBA -InputFile $file.FullName -QueryTimeout 300
+        Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -InputFile $file.FullName -QueryTimeout 300
     }
     #Then scalar functions
     Write-Verbose "`n        ***Creating/Updating Scalar Functions `n"
     $fileList = Get-ChildItem -Path .\functions-scalar -Recurse
     Foreach ($file in $fileList){
         Write-Verbose $file.FullName
-        Invoke-Sqlcmd -ServerInstance $instance -Database DBA -InputFile $file.FullName
+        Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -InputFile $file.FullName
     }
     #Then TVFs
     Write-Verbose "`n        ***Creating/Updating Table-Valued Functions `n"
     $fileList = Get-ChildItem -Path .\functions-tvfs -Recurse
     Foreach ($file in $fileList){
         Write-Verbose $file.FullName
-        Invoke-Sqlcmd -ServerInstance $instance -Database DBA -InputFile $file.FullName
+        Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -InputFile $file.FullName
     }
     #Then Procedures
     Write-Verbose "`n        ***Creating/Updating Stored Procedures `n"
     $fileList = Get-ChildItem -Path .\stored-procedures -Recurse -Filter *.sql
     Foreach ($file in $fileList){
         Write-Verbose $file.FullName
-        Invoke-Sqlcmd -ServerInstance $instance -Database DBA -InputFile $file.FullName
+        Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -InputFile $file.FullName
     }
-    #Then First Responder Kit
-    Write-Verbose "`n        ***Creating/Updating First Responder Kit `n"
-    $fileList = Get-ChildItem -Path .\oss\firstresponderkit -Recurse -Filter *.sql
-    Foreach ($file in $fileList){
-        Write-Verbose $file.FullName
-        Invoke-Sqlcmd -ServerInstance $instance -Database DBA -InputFile $file.FullName
+    #Skip Open Source procedures if asked
+    If ($SkipOSS -eq $false){
+        #Then First Responder Kit
+        Write-Verbose "`n        ***Creating/Updating First Responder Kit `n"
+        $fileList = Get-ChildItem -Path .\oss\firstresponderkit -Recurse -Filter *.sql
+        Foreach ($file in $fileList){
+            Write-Verbose $file.FullName
+            Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -InputFile $file.FullName
+        }
+        #Then sp_whoisactive
+        Write-Verbose "`n        ***Creating/Updating sp_WhoIsActive `n"
+        $fileList = Get-ChildItem -Path .\oss\whoisactive -Recurse -Filter *.sql
+        Foreach ($file in $fileList){
+            Write-Verbose $file.FullName
+            Invoke-Sqlcmd -ServerInstance $instance -Database master -InputFile $file.FullName
+        }
+        ## WOO HOO! Ola's code is idempotent now!
+        Write-Verbose "`n        ***Creating/Updating Ola Hallengren Maintenance Solution `n"
+        $fileList = Get-ChildItem -Path .\oss\olahallengren -Recurse -Filter *.sql
+        Foreach ($file in $fileList){
+            Write-Verbose $file.FullName
+            Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -InputFile $file.FullName
+        }
     }
-     #Then sp_whoisactive
-     Write-Verbose "`n        ***Creating/Updating sp_WhoIsActive `n"
-     $fileList = Get-ChildItem -Path .\oss\whoisactive -Recurse -Filter *.sql
-     Foreach ($file in $fileList){
-         Write-Verbose $file.FullName
-         Invoke-Sqlcmd -ServerInstance $instance -Database master -InputFile $file.FullName
-     }
-     ## WOO HOO! Ola's code isn't idempotent now!
-     Write-Verbose "`n        ***Creating/Updating Ola Hallengren Maintenance Solution `n"
-     $fileList = Get-ChildItem -Path .\oss\olahallengren -Recurse -Filter *.sql
-     Foreach ($file in $fileList){
-         Write-Verbose $file.FullName
-         Invoke-Sqlcmd -ServerInstance $instance -Database DBA -InputFile $file.FullName
-     }
+
 
 #That's it!
 }
--- create-database.sql
+++ create-database.sql
@@ -1,24 +1,24 @@
 --Create DB
-IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DBA')
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = '$(DbName)')
 BEGIN
-    CREATE DATABASE [DBA];
-    ALTER DATABASE  [DBA] ADD FILEGROUP [DATA];
+    CREATE DATABASE [$(DbName)];
+    ALTER DATABASE  [$(DbName)] ADD FILEGROUP [DATA];
     --Need to use dynamic SQL to ensure we put this data file in the right spot dynamically
     DECLARE @sql nvarchar(max);
-    SELECT @sql = N'ALTER DATABASE  [DBA] ADD FILE (NAME=''DBA_data'', FILENAME=''' 
+    SELECT @sql = N'ALTER DATABASE  [$(DbName)] ADD FILE (NAME=''$(DbName)_data'', FILENAME=''' 
                     + LEFT(physical_name,LEN(physical_name)-CHARINDEX('\',REVERSE(physical_name))+1) 
-                    + N'DBA_data.ndf' + N''') TO FILEGROUP [DATA];'
+                    + N'$(DbName)_data.ndf' + N''') TO FILEGROUP [DATA];'
     FROM sys.master_files 
-    WHERE database_id = db_id('DBA')
+    WHERE database_id = db_id('$(DbName)')
     AND file_id = 1;
     EXEC sp_executesql @statement = @sql;
     --And now finish up creating it
-    ALTER DATABASE  [DBA] MODIFY FILEGROUP [DATA] DEFAULT;
-    ALTER DATABASE  [DBA] SET READ_COMMITTED_SNAPSHOT ON;
+    ALTER DATABASE  [$(DbName)] MODIFY FILEGROUP [DATA] DEFAULT;
+    ALTER DATABASE  [$(DbName)] SET READ_COMMITTED_SNAPSHOT ON;
     --set sa as owner
-    ALTER AUTHORIZATION ON database::DBA TO sa;
+    ALTER AUTHORIZATION ON database::$(DbName) TO sa;
     --set to simple recovery on creation
     --if you change this after creation, we won't change it back.
-    ALTER DATABASE DBA SET RECOVERY SIMPLE;
+    ALTER DATABASE $(DbName) SET RECOVERY SIMPLE;
 END
 GO
