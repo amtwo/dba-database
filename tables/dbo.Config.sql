90d807791f8b8525dda669aae02d86b949e15a6f
Andy Mallon <amallon@wayfair.com>
2020-06-09 10:04:41 -0400

    Update dbo.Config.sql

--- tables/dbo.Config.sql
+++ tables/dbo.Config.sql
@@ -29,3 +29,33 @@ BEGIN
     INSERT INTO dbo.Config (ConfigCode, ConfigCategory, ConfigName, VarbinaryValue)
     VALUES ('REPLSCMOPT', 'Replication', 'Schema Option', 0x00000044080350DF)
 END;
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Config WHERE ConfigCode = 'EMAILBOLDCOLOR')
+BEGIN
+    INSERT INTO dbo.Config (ConfigCode, ConfigCategory, ConfigName, UnicodeValue)
+    VALUES ('EMAILBOLDCOLOR', 'Alerting', 'Email Bold Color', '#032E57')
+END;
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Config WHERE ConfigCode = 'EMAILALERTCOLOR')
+BEGIN
+    INSERT INTO dbo.Config (ConfigCode, ConfigCategory, ConfigName, UnicodeValue)
+    VALUES ('EMAILALERTCOLOR', 'Alerting', 'Email Alert Color', '#DC080A')
+END;
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Config WHERE ConfigCode = 'EMAILBGCOLOR')
+BEGIN
+    INSERT INTO dbo.Config (ConfigCode, ConfigCategory, ConfigName, UnicodeValue)
+    VALUES ('EMAILBGCOLOR', 'Alerting', 'Email Background Color', '#D0CAC4')
+END;
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Config WHERE ConfigCode = 'EMAILBGCOLOR2')
+BEGIN
+    INSERT INTO dbo.Config (ConfigCode, ConfigCategory, ConfigName, UnicodeValue)
+    VALUES ('EMAILBGCOLOR2', 'Alerting', 'Email Background Color (Alternate)', '#E4F1FE')
+END;
+
+IF NOT EXISTS (SELECT 1 FROM dbo.Config WHERE ConfigCode = 'EMAILALRTBGCOLOR')
+BEGIN
+    INSERT INTO dbo.Config (ConfigCode, ConfigCategory, ConfigName, UnicodeValue)
+    VALUES ('EMAILALRTBGCOLOR', 'Alerting', 'Email Background Color Alert', '#FEF1E4')
+END;
