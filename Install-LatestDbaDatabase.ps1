8034576169092433f67250fdd8b74a33c5f91c1f
Andy Mallon <amtwo@users.noreply.github.com>
2021-04-27 11:29:36 -0400

    Update Install-LatestDbaDatabase.ps1
    Fixing the verbose output to give proper shoutout to @erikdarlingdata data daddy's dandy troubleshooting scripts.

--- Install-LatestDbaDatabase.ps1
+++ Install-LatestDbaDatabase.ps1
@@ -112,7 +112,7 @@ foreach($instance in $InstanceName) {
             Invoke-Sqlcmd -ServerInstance $instance -Database $DatabaseName -InputFile $file.FullName
         }
         ## That Erik. He's such a Darling.
-        Write-Verbose "`n        ***Creating/Updating Ola Hallengren Maintenance Solution `n"
+        Write-Verbose "`n        ***Creating/Updating Darling's Dandy Data Troubleshooting scripts `n"
         $fileList = Get-ChildItem -Path .\oss\darlingdata -Recurse -Filter *.sql
         Foreach ($file in $fileList){
             Write-Verbose $file.FullName
